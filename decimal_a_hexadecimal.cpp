b8952a0381a928143ab2e818521daea2345225c1
Mich-007 <michellegutierreze@hotmail.com>
2021-11-13 00:53:41 -0600

    ejercicio 2

---
+++ decimal_a_hexadecimal.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int digito_hexadecimal[100];
+int decimal, residuo, base, resultado, i = 0;
+void convertir(int numero, int digito);
+
+void convertir(int numero, int digito){
+    int decimal = numero;
+    int base = digito;
+    do
+    {
+        resultado = decimal / base;
+        residuo = decimal % base;
+        digito_hexadecimal[i] = residuo;
+        decimal = resultado;
+        i++;
+    } while (resultado > (base-1));
+
+    digito_hexadecimal[i] = resultado;
+
+    cout << "El equivalente en hexadecimal es: ";
+
+    for (int j = i; j >= 0; j--)
+    {
+        if (digito_hexadecimal[j] == 10)
+        {
+            cout << "A";
+        }
+        else if (digito_hexadecimal[j] == 11)
+        {
+            cout << "B";
+        }
+        else if (digito_hexadecimal[j] == 12)
+        {
+            cout << "C";
+        }
+        else if (digito_hexadecimal[j] == 13)
+        {
+            cout << "D";
+        }
+        else if (digito_hexadecimal[j] == 14)
+        {
+            cout << "E";
+        }
+        else if (digito_hexadecimal[j] == 15)
+        {
+            cout << "F";
+        }
+        else
+        {
+            cout << digito_hexadecimal[j];
+        }   
+    }
+    cout << endl;
+
+
+}
+
+int main()
+{
+
+    cout<<"*************************************************"<<endl;
+    cout<<"    Convertidor de decimal a Hexadecimal"<<endl;
+
+    cout<<"\nIngrese la base que desea mayor a 10: "<<endl;
+    cin >> base;
+
+    cout << "\nIngrese el numero decimal: " << endl;
+    cin >> decimal;
+
+    convertir(decimal, base);
+}
